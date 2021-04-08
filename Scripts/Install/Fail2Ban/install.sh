bb13bc35d5b7c0856d4c88631bf784ad9e9366ed
Neuralis-AI <64447278+Neuralis-AI@users.noreply.github.com>
2021-04-09 00:05:53 +0200

    Update install.sh

--- Scripts/Install/Fail2Ban/install.sh
+++ Scripts/Install/Fail2Ban/install.sh
@@ -3,9 +3,9 @@
 # Ubuntu, Red Hat, CentOS
 apt-get update
 apt install fail2ban -y
-wget -O /etc/fail2ban/jail.local
-wget -O /etc/fail2ban/action.d/iptables-common.local
-chmod 744 /etc/fail2ban/jail.local
+wget -O /etc/fail2ban/jail.local https://raw.githubusercontent.com/Neuralis-AI/Hosting/main/Scripts/Install/Fail2Ban/jail.local
+wget -O /etc/fail2ban/action.d/iptables-common.local https://raw.githubusercontent.com/Neuralis-AI/Hosting/main/Scripts/Install/Fail2Ban/iptables-common.local
+chmod 744 /etc/fail2ban/jail.local 
 chmod 744 /etc/fail2ban/action.d/iptables-common.local
 systemctl restart fail2ban
 exit
